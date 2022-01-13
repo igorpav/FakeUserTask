f5281d0aa9215e4ba78b65c9ceb1caf9b9ec3825
igorpav <Igor.Pavlichenkov@tatar.ru>
2022-01-13 12:12:59 +0300

    Initial Commit

---
+++ FakeUserTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeUserTask
+//
+//  Created by Игорь Павличенков on 13.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
